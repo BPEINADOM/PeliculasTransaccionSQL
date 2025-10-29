da1d3213c22baf358c16c90d0cdcf98599df1f16
BPEINADOM <bpeinadom@unicartagena.edu.co>
2025-10-28 22:10:34 -0500

    Creacion del Proyecto Maven.

---
+++ peliculas/src/main/java/co/edu/udec/peliculas/App.java
@@ -0,0 +1,13 @@
+package co.edu.udec.peliculas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
